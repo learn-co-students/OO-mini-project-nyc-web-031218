ef5c49b237a82b467e5c3dd1d52fc3b7fd880d3e
harim1206 <harim1206@gmail.com>
2018-03-22 14:05:28 -0400

    top_three_recipes

--- app/models/User.rb
+++ app/models/User.rb
@@ -12,15 +12,15 @@
     end
 
     def self.all
-      @all
+      @@all
     end
 
     def self.count
       @@count
     end
 
-    def add_recipe_card(recipe, date, rating)
-      rc = Recipecard.new(date, rating)
+    def add_recipe_card(recipe, rating, date)
+      rc = Recipecard.new(rating, date)
       rc.user = self
       rc.recipe = recipe
       @recipes << recipe
@@ -29,10 +29,53 @@
     end
 
 
-    # def recipes
-    #   recipecard collect do |recipecard|
-    #     recipecard.recipe
-    #   end
-    # end
+    def top_three_recipes
+      user_recipe_cards = []
+
+      Recipecard.all.each do |rc|
+        if rc.user == self
+          user_recipe_cards << rc
+        end
+      end
+
+      #user_recipe_cards is an array of all recipe cards
+      #for this user
+      user_recipe_cards
+
+      user_recipe_cards.each do |rc|
+
+      end
+
+      #user_recipecards => [recipe cards that belong to user]
+
+      #[Pizza, Pasta, Salad]
+      # self.recipes.each do |recipe|
+      #
+      #
+      # end
+
+
+    end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,25 @@
 require_relative '../config/environment.rb'
 
+
+mark = User.new("Mark")
+harim = User.new("Harim")
+rani = User.new("Rani")
+
+pizza  = Recipe.new("Pizza")
+pasta  = Recipe.new("Pasta")
+salad = Recipe.new("Salad")
+burger = Recipe.new("Burger")
+
+
+harim.add_recipe_card(pasta, 4, "June")
+harim.add_recipe_card(pizza, 2, "July")
+harim.add_recipe_card(salad, 8, "May")
+harim.add_recipe_card(burger, 7, "April")
+rani.add_recipe_card(pasta, 5, "June")
+rani.add_recipe_card(pizza, 2, "Dec")
+mark.add_recipe_card(burger, 5, "June")
+mark.add_recipe_card(salad, 7, "Nov")
+
+harim_recipe_cards = harim.top_three_recipes
+
 binding.pry
