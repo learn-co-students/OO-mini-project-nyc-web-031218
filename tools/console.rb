9a3cab7fdc2d7ae8d496061976ae2ea0dad08ad3
J.D. Bean <jonathandbean@gmail.com>
2018-03-22 15:04:57 -0400

    improved 'test' readability

--- tools/console.rb
+++ tools/console.rb
@@ -3,17 +3,17 @@ require_relative '../config/environment.rb'
 
 puts "## GENERAL PUPROSE METHODS".colorize(:magenta)
 
-puts "# Initializations".colorize(:yellow)
+puts "  # Initializations".colorize(:yellow)
 pasta = Recipe.new("Pasta")
 pizza = Recipe.new("Pizza")
 meatloaf = Recipe.new("Meatloaf")
 chicken = Recipe.new("Chicken")
-puts "created new recipes: Pasta, Pizza, Meatload, Chicken".colorize(:green)
+puts "    created new recipes: Pasta, Pizza, Meatload, Chicken".colorize(:green)
 
 tom = User.new("Tom")
 dick = User.new("Dick")
 harry = User.new("Harry")
-puts "created new users: Tom, Dick, Harry".colorize(:green)
+puts "    created new users: Tom, Dick, Harry".colorize(:green)
 
 card1 = harry.add_recipe_card(pasta, 12, 3)
 card2 = tom.add_recipe_card(meatloaf, 17, 4)
@@ -24,26 +24,26 @@ card6 = harry.add_recipe_card(pizza, 12, 3)
 card7 = tom.add_recipe_card(chicken, 12, 3)
 card8 = harry.add_recipe_card(chicken, 12, 2)
 card9 = dick.add_recipe_card(pasta, 12, 5)
-puts "User#add_recipe_card: added card(1..9)".colorize(:green)
+puts "    User#add_recipe_card: added card(1..9)".colorize(:green)
 
-puts "# CLASS.all (x6)".colorize(:yellow)
+puts "  # CLASS.all (x6)".colorize(:yellow)
 
 if User.all == [tom, dick, harry]
-  puts "User.all returns array of all users".colorize(:green)
+  puts "   User.all returns array of all users".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "   FAILED TEST".colorize(:red)
 end
 
 if Recipe.all == [pasta, pizza, meatloaf, chicken]
-  puts "Recipe.all returns array of all recipes".colorize(:green)
+  puts "   Recipe.all returns array of all recipes".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "   FAILED TEST".colorize(:red)
 end
 
 if RecipeCard.all == [card1, card2, card3, card4, card5, card6, card7, card8, card9]
-  puts "RecipeCard.all returns array of all recipe cards".colorize(:green)
+  puts "   RecipeCard.all returns array of all recipe cards".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 # if Ing:redient.all ==
@@ -58,12 +58,12 @@ end
 puts "## RECIPE <-> RECIPE_CARD <-> USER".colorize(:magenta)
 
 
-puts "User Test".colorize(:yellow)
+puts "  User Test".colorize(:yellow)
 
 if harry.top_three_recipes == [meatloaf, pasta, pizza]
-  puts "user #top_three_recipes returns array of user's top three recipies".colorize(:green)
+  puts "    user #top_three_recipes returns array of user's top three recipies".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 # if harry.most_recent_recipe ==
@@ -72,44 +72,44 @@ end
 #   puts "FAILED TEST".colorize(:red)
 # end
 
-puts "Recipe Tests".colorize(:yellow)
+puts "  Recipe Tests".colorize(:yellow)
 #
 if Recipe.most_popular == pasta
-  puts "Recipe.most_popular returns most popular recipe".colorize(:green)
+  puts "    Recipe.most_popular returns most popular recipe".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 if pizza.users == [tom, harry]
-  puts "recipe .users returns recipe's users".colorize(:green)
+  puts "    recipe .users returns recipe's users".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
-puts "RecipeCard Tests".colorize(:yellow)
+puts "  RecipeCard Tests".colorize(:yellow)
 
 if card1.user == harry
-  puts "recipecard .user returns recipecard's user".colorize(:green)
+  puts "    recipecard .user returns recipecard's user".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 if card1.recipe == pasta
-  puts "recipecard .recipe returns recipecard's recipe".colorize(:green)
+  puts "    recipecard .recipe returns recipecard's recipe".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 if card1.date == 12
-  puts "recipecard .date returns recipecard's date".colorize(:green)
+  puts "    recipecard .date returns recipecard's date".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 if card1.rating == 3
-  puts "recipecard .rating returns recipecard's rating".colorize(:green)
+  puts "    recipecard .rating returns recipecard's rating".colorize(:green)
 else
-  puts "FAILED TEST".colorize(:red)
+  puts "    FAILED TEST".colorize(:red)
 end
 
 #card1 = harry.add_recipe_card(pasta, 12, 3)
