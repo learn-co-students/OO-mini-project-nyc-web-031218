40ad2da6126c7556caa57b7add86121542410add
Jon <jbvaldesuso@gmail.com>
2018-03-22 13:31:29 -0400

    yeah

--- app/models/User.rb
+++ app/models/User.rb
@@ -29,11 +29,21 @@ class User
   end
 
   def top_three_recipes
+    user_recipes = get_users_recipes
+    sorted_recipes = user_recipes.sort_by do |recipe|
+      recipe.rating
+    end.reverse
+    sorted_recipes[0..2]
+  end
 
+  def get_users_recipes
+    RecipeCard.all.select do |rc|
+      rc.user = self
+    end
   end
 
   def most_recent_recipes
-
+    get_users_recipes[-1]
   end
 
 end
